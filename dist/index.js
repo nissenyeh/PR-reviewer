4d89e56f95ab8e49a982ed15509dd526d40f8a95
nissen <nishan.yeh@junyiacademy.org>
2024-05-01 05:11:54 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -34261,7 +34261,7 @@ async function main() {
       core.info(`Pull Request Title: ${pr.title}`);
       // const diffContent = await getDiffContent(pr.diff_url)
       // core.info(diffContent.data);
-      const baseBranch = pr.repo.default_branch
+      const baseBranch = pr.base.ref
       core.info(`baseBranch: ${baseBranch}`);
 
       const searchPath = pr.head.ref
