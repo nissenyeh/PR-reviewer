2cd1b058e4d671bb86f10ecaa164d0f0d99235ea
nissen <nishan.yeh@junyiacademy.org>
2024-05-01 04:03:04 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -33766,7 +33766,10 @@ const {
   formatTeamsMessage,
 } = __nccwpck_require__(3505);
 
+
+// fix: GITHUB_TOKEN 拿不到
 const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_API_URL } = process.env;
+
 const AUTH_HEADER = {
   Authorization: `token ${GITHUB_TOKEN}`,
 };
@@ -33785,6 +33788,38 @@ async function getPullRequests() {
   });
 }
 
+function getDateXDaysAgo(days) {
+  const date = new Date();
+  date.setDate(date.getDate() - days);
+  return date.toISOString().split('T')[0]; // 返回格式為 YYYY-MM-DD
+}
+
+const SEARCH_ENDPOINT = `${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/pulls`;
+
+async function getOldPullRequests(days) {
+
+  const query = `repo:${GITHUB_REPOSITORY} is:pr is:open created:<${getDateXDaysAgo(days)}`;
+  const headers = {
+    Authorization: `token ${GITHUB_TOKEN}`,
+    'Accept': 'application/vnd.github.v3+json'
+  };
+
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: SEARCH_ENDPOINT,
+      headers: headers,
+      params: {
+        q: query
+      }
+    });
+    return response.data.items; // 返回的是符合條件的 PR 列表
+  } catch (error) {
+    console.error('Error fetching old pull requests:', error);
+    return null;
+  }
+}
+
 
 /**
  * Main function for the GitHub Action
@@ -33792,12 +33827,13 @@ async function getPullRequests() {
 async function main() {
   try {
     core.info('Starting...');
-    core.info(PULLS_ENDPOINT);
     core.info('Getting open pull requests...');
-    core.info(GITHUB_TOKEN)
-    core.info(GITHUB_REPOSITORY)
-    core.info(GITHUB_API_URL)
-    const pullRequests = await getPullRequests();
+
+    const openTime = core.getInput('open-time');
+    core.info('openTime...');
+    core.info(openTime);
+    const pullRequests = await getOldPullRequests(openTime);
+    // const pullRequests = await getPullRequests();
     const totalReviewers = await getPullRequestsReviewersCount(pullRequests.data);
     core.info(`There are ${pullRequests.data.length} open pull requests and ${totalReviewers} reviewers`);
     const pullRequestsToReview = getPullRequestsToReview(pullRequests.data);
